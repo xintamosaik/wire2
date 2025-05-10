a12b0395b616c69fff8632b56338a2cd6592f504
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-05-10 14:08:23 +0200

    feat: add node marking and connection functionality

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ function loadFromLocalStorage(): Node[] {
 function getUUID() {
   return Date.now().toString(36) + Math.random().toString(36).slice(2)
 }
+
+
 const initialNodes: Node[] = [
   { id: getUUID(), label: 'unnamed', connections: [] },
   { id: getUUID(), label: 'unnamed', connections: [] },
@@ -26,6 +28,7 @@ const initialNodes: Node[] = [
 ]
 function App() {
   const [nodes, setNodes] = useState<Node[]>(loadFromLocalStorage().length ? loadFromLocalStorage() : initialNodes)
+  const [markedNode, setMarkedNode] = useState<string | null>(null)
   return (
     <>
       <h1>Wire2</h1>
@@ -37,18 +40,47 @@ function App() {
         <button onClick={() => setNodes([...nodes, { id: getUUID(), label: `unnamed`, connections: [] }])}>
           Add Node
         </button>
+        <output id="currentlyMarkedNodes">
+          {markedNode ? `Currently marked node: ${markedNode}` : 'No node marked'}
+        </output>
       </div>
 
       <output style={{ display: 'grid', gap: '1rem', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr' }}>
         {nodes.map((node) => (
           <div key={node.id} className="node" style={{ fontFamily: "monospace", border: '1px solid white', padding: '1rem', borderRadius: '5px' }}>
 
-            <h2>{node.label}</h2>
-            <h3>{node.id}</h3>
-            <p>Connections: {node.connections.join(', ')}</p>
-          </div>
+      
+            <h2>{node.id}</h2>
+            <h3>Connections</h3>
+           
+            <button onClick={() => {
+              if (markedNode) {
+                setNodes(nodes.map((n) => {
+                  if (n.id === markedNode) {
+                    return { ...n, connections: [...n.connections, node.id] }
+                  }
+                  return n
+                }))
+                setMarkedNode(null)
+              } else {
+                setMarkedNode(node.id)
+              }
+              console.log(node.id)
+          
+            }}>
+
+              mark for connection
+            </button>
+            <ul>
+              {node.connections.map((connection) => (
+                <li key={connection}>{connection}</li>
+              ))}
+            </ul>
+        </div>
         ))}
+          
       </output>
+  
 
 
     </>
