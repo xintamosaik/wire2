9ada6f42b73f8244b128065ee9a0b05832c43512
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-05-10 15:14:48 +0200

    make it easier to read

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,29 @@ import { useState } from 'react'
 interface Node {
   id: string
   label: string
-  connections: string[]
+  connections: Set<string>
 }
 function saveToLocalStorage(nodes: Node[]) {
-  localStorage.setItem('nodes', JSON.stringify(nodes))
+  const converted = nodes.map(node => ({
+    ...node,
+    connections: Array.from(node.connections),
+  }))
+  localStorage.setItem('nodes', JSON.stringify(converted))
+  console.log("saved to local storage")
 }
 
 function loadFromLocalStorage(): Node[] {
   const nodes = localStorage.getItem('nodes')
   if (nodes) {
-    return JSON.parse(nodes)
+     return JSON.parse(nodes).map((node: Node) => ({
+      ...node,
+      connections: node.connections ? new Set<string>(node.connections) : new Set<string>(),
+    }));
   }
   return []
+}
+function clearLocalStorage() {
+  localStorage.removeItem('nodes')
 }
 function getUUID() {
   return Date.now().toString(36) + Math.random().toString(36).slice(2)
@@ -22,9 +33,9 @@ function getUUID() {
 
 
 const initialNodes: Node[] = [
-  { id: getUUID(), label: 'unnamed', connections: [] },
-  { id: getUUID(), label: 'unnamed', connections: [] },
-  { id: getUUID(), label: 'unnamed', connections: [] },
+  { id: getUUID(), label: 'unnamed', connections: new Set() },
+  { id: getUUID(), label: 'unnamed', connections: new Set() },
+  { id: getUUID(), label: 'unnamed', connections: new Set() },
 ]
 function App() {
   const [nodes, setNodes] = useState<Node[]>(loadFromLocalStorage().length ? loadFromLocalStorage() : initialNodes)
@@ -37,9 +48,17 @@ function App() {
           saveToLocalStorage(nodes)
 
         }}>Save</button>
-        <button onClick={() => setNodes([...nodes, { id: getUUID(), label: `unnamed`, connections: [] }])}>
+
+        <button onClick={() => {
+          clearLocalStorage()
+          setNodes([])
+        }}>Clear</button>
+
+        <button onClick={() => setNodes([...nodes, { id: getUUID(), label: `unnamed`, connections: new Set() }])}>
           Add Node
         </button>
+
+
         <output id="currentlyMarkedNodes">
           {markedNode ? `Currently marked node: ${markedNode}` : 'No node marked'}
         </output>
@@ -49,38 +68,50 @@ function App() {
         {nodes.map((node) => (
           <div key={node.id} className="node" style={{ fontFamily: "monospace", border: '1px solid white', padding: '1rem', borderRadius: '5px' }}>
 
-      
+
             <h2>{node.id}</h2>
             <h3>Connections</h3>
-           
+
             <button onClick={() => {
-              if (markedNode) {
-                setNodes(nodes.map((n) => {
-                  if (n.id === markedNode) {
-                    return { ...n, connections: [...n.connections, node.id] }
-                  }
-                  return n
-                }))
-                setMarkedNode(null)
-              } else {
+              if (!markedNode) {
                 setMarkedNode(node.id)
+                return;
               }
+
+              if (markedNode === node.id) {
+                setMarkedNode(null)
+                return;
+              }
+
+              nodes.forEach((entry) => {
+                if (markedNode === entry.id) {
+                  console.log("found marked node")
+                  console.log(entry)
+                  entry.connections.add(node.id)
+
+                }
+
+              })
+
+
+              setMarkedNode(null)
               console.log(node.id)
-          
+
             }}>
 
               mark for connection
             </button>
             <ul>
-              {node.connections.map((connection) => (
-                <li key={connection}>{connection}</li>
+              {node.connections.size === 0 ? <li>no connections</li> : null}
+              {Array.from(node.connections).map((connection) => (
+                <li key={"li_" + connection}>{connection}</li>
               ))}
             </ul>
-        </div>
+          </div>
         ))}
-          
+
       </output>
-  
+
 
 
     </>
